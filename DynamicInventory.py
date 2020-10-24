48f211f6a560863e8c7129f2ddb4f87fe8e8bcf8
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2020-10-23 22:56:23 -0400

    Update DynamicInventory.py

--- DynamicInventory.py
+++ DynamicInventory.py
@@ -1,7 +1,7 @@
 #!/bin/python
 # Shebang Line Should Point Python Installation Path
 """
-Author Mithun Technologies
+Author Landmark Technologies
 Fetech List of Servers from AWS and group based on EC2
 instance tag name and value.
 """
